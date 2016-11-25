c9eb5571cf1955cf339e067472034b4b8ea6c204
Rupam <Rupam@Rupam-PC>
2016-11-24 21:34:18 -0800

    Added bubleSort in Main

---
+++ BubbleSort/src/com/Cybernatic/clsBubleSort.java
@@ -0,0 +1,22 @@
+package com.Cybernatic;
+
+public abstract class clsBubleSort {
+	
+	public int[] bubleSort(int intArray[]){
+        int intLength = intArray.length;
+        int temp = 0;
+       
+        for(int i=0; i < intLength; i++){
+                for(int j=1; j < (intLength-i); j++){
+                        if(intArray[j-1] > intArray[j]){
+                            //swap the elements!
+                            temp = intArray[j-1];
+                            intArray[j-1] = intArray[j];
+                            intArray[j] = temp;
+                        }
+                }
+        }
+		
+		return intArray;
+	}
+}
---
+++ BubbleSort/src/com/Main/clsMain.java
@@ -0,0 +1,43 @@
+package com.Main;
+//import com.Cybernatic.clsBubleSort;
+
+public class clsMain {
+
+	public static void main(String[] args) {
+	    int intArray[] = new int[]{5,90,35,45,150,3};
+	    int intTemp[];
+	    
+//	    clsMain mn = new clsMain();
+//	    intTemp = mn.bubleSort(intArray);
+
+	    intTemp = bubleSort(intArray);
+	    
+        System.out.println("After Bubble Sort");
+        for(int i=0; i < intTemp.length; i++){
+                System.out.print(intTemp[i] + " ");
+        }
+	}
+
+	
+	public static int[] bubleSort(int intArray[]){
+        int intLength = intArray.length;
+        int temp = 0;
+       
+        for(int i=0; i < intLength; i++){
+                for(int j=1; j < (intLength-i); j++){
+                        if(intArray[j-1] > intArray[j]){
+                            //swap the elements!
+                            temp = intArray[j-1];
+                            intArray[j-1] = intArray[j];
+                            intArray[j] = temp;
+                        }
+                }
+        }
+		
+		return intArray;
+	}
+	
+	
+	
+
+}
