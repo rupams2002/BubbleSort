6e3649f0525edfc3d9efd1604fad9a856c0dd417
Rupam <Rupam@Rupam-PC>
2016-11-25 12:03:58 -0800

    Modified bubbleSort V2.0

--- BubbleSort/src/com/Main/clsMain.java
+++ BubbleSort/src/com/Main/clsMain.java
@@ -19,6 +19,24 @@ public class clsMain {
 	}
 
 	
+//	public static int[] bubleSort(int intArray[]){
+//        int intLength = intArray.length;
+//        int temp = 0;
+//       
+//        for(int i=0; i < intLength; i++){
+//                for(int j=1; j < (intLength-i); j++){
+//                        if(intArray[j-1] > intArray[j]){
+//                            //swap the elements!
+//                            temp = intArray[j-1];
+//                            intArray[j-1] = intArray[j];
+//                            intArray[j] = temp;
+//                        }
+//                }
+//        }
+//		
+//		return intArray;
+//	}
+	
 	public static int[] bubleSort(int intArray[]){
         int intLength = intArray.length;
         int temp = 0;
@@ -36,8 +54,5 @@ public class clsMain {
 		
 		return intArray;
 	}
-	
-	
-	
 
 }
