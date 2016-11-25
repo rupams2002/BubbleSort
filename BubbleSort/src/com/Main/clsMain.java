a4c1444da88da708612f29ba3190c850b09b9502
Rupam <Rupam@Rupam-PC>
2016-11-25 12:11:50 -0800

    Modified bubleSort V3.0

--- BubbleSort/src/com/Main/clsMain.java
+++ BubbleSort/src/com/Main/clsMain.java
@@ -19,24 +19,6 @@ public class clsMain {
 	}
 
 	
-//	public static int[] bubleSort(int intArray[]){
-//        int intLength = intArray.length;
-//        int temp = 0;
-//       
-//        for(int i=0; i < intLength; i++){
-//                for(int j=1; j < (intLength-i); j++){
-//                        if(intArray[j-1] > intArray[j]){
-//                            //swap the elements!
-//                            temp = intArray[j-1];
-//                            intArray[j-1] = intArray[j];
-//                            intArray[j] = temp;
-//                        }
-//                }
-//        }
-//		
-//		return intArray;
-//	}
-	
 	public static int[] bubleSort(int intArray[]){
         int intLength = intArray.length;
         int temp = 0;
@@ -54,5 +36,23 @@ public class clsMain {
 		
 		return intArray;
 	}
+	
+//	public static int[] bubleSort(int intArray[]){
+//        int intLength = intArray.length;
+//        int temp = 0;
+//       
+//        for(int i=0; i < intLength; i++){
+//                for(int j=1; j < (intLength-i); j++){
+//                        if(intArray[j-1] > intArray[j]){
+//                            //swap the elements!
+//                            temp = intArray[j-1];
+//                            intArray[j-1] = intArray[j];
+//                            intArray[j] = temp;
+//                        }
+//                }
+//        }
+//		
+//		return intArray;
+//	}
 
 }
